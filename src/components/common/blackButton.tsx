3d28acad1947f419b984e4c226e0fb534969b475
Varun <aitvarunbhatia@gmail.com>
2024-09-09 20:47:40 +0530

    updated border

--- src/components/common/blackButton.tsx
+++ src/components/common/blackButton.tsx
@@ -1,7 +1,7 @@
 export default function BlackButton({ text }: { text: string }) {
   return (
     <div className="relative inline-block group">
-      <button className="relative z-10 bg-[#fbf7e9] text-[#363636] text-xs md:text-base font-bold py-3 px-6 rounded-lg hover:bg-[#fdfbf2] transition duration-300 ease-in-out transform group-hover:translate-x-[-2px] group-hover:translate-y-[-2px] uppercase tracking-wide">
+      <button className="relative z-10 bg-[#fbf7e9] text-[#363636] text-xs md:text-base font-bold py-3 px-6 rounded-lg hover:bg-[#fdfbf2] transition duration-300 ease-in-out transform group-hover:translate-x-[-2px] group-hover:translate-y-[-2px] uppercase tracking-wide border border-black">
         {text.toUpperCase()}
       </button>
       <div className="absolute inset-0 bg-[#363636] rounded-lg transition-all duration-300 ease-in-out transform translate-x-[4px] translate-y-[4px] group-hover:translate-x-[6px] group-hover:translate-y-[6px]"></div>
